e2c2f1e7bc129e57929fb34eb34eaa331ece0d19
JcAPC <jaycemarkpresbitero1902@gmail.com>
2025-01-27 22:40:23 +0800

    Added personalprofile

---
+++ vue-project/src/components/PersonalProfile.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <!-- Hero Section -->
+<div id="home" class="text-center">
+    <div>
+        <div>
+            <div>
+                <h1>Welcome to My Midterm Project</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- About Me Section -->
+<div id="about">
+    <h2>About Me</h2>
+    <div>
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <h5>Achievement</h5>
+                        <p>
+                            - 1st place in "Best in Cinematography"<br>
+                            - With Honors in Grade 12
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <h5>Personal Information</h5>
+                        <p>
+                            - Jesmark David C. Presbitero<br>
+                            - From Lubao, Pampanga<br>
+                            - Born on July 19, 2004<br>
+                            - Currently 20 years old
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div>
+                <div>
+                    <h5>Hobbies</h5>
+                    <p>
+                        - Driving and Motorcycles<br>
+                        - Playing Games<br>
+                        - Petting Cats
+                    </p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <h5>Dreams</h5>
+                    <p>
+                        - Make my parents proud<br>
+                        - Succeed in life<br>
+                        - Help people in need<br>
+                        - Become a more religious person
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+</template>
+
+
+<script></script>
+
+<style></style>
