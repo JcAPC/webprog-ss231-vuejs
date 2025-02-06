66745908fae10e57175a4684ef34e89afec54124
JcAPC <jaycemarkpresbitero1902@gmail.com>
2025-02-06 09:55:16 +0000

    yes

--- vue-project/src/lib/supabaseClient.js
+++ vue-project/src/lib/supabaseClient.js
@@ -1,3 +1,6 @@
+
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://jggkkwfiakguntovsbdd.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpnZ2trd2ZpYWtndW50b3ZzYmRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgzMTYwMTUsImV4cCI6MjA1Mzg5MjAxNX0.gbMg6kngVX_BIRwHqPOtHuhr-MR4xCIoM8snbNAFuNQ')
+const supabaseUrl = 'https://jggkkwfiakguntovsbdd.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
